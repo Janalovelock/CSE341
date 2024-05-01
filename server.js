de3916ba9a74d516bcb2740677d1f5e6598142c4
Janalovelock <janalovelock@gmail.com>
2024-05-01 15:29:21 -0500

    fixed main

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const mongodb = require('./db/connect');
 const nameRoute = require('./routes/nameRoute');
 const PORT = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//     res.send( `
-//     <html>
-//         <head>
-//             <title>Home Page</title>
-//         </head>
-//         <body>
-//             <h1>Welcome to Jana Lovelock's Express web server!</h1>
-//             <p>Click <a href="/name">here</a> to go to the /name directory.</p>
-//         </body>
-//     </html>
-// `);
-// });
+app.get('/', (req, res) => {
+    res.send( `
+    <html>
+        <head>
+            <title>Home Page</title>
+        </head>
+        <body>
+            <h1>Welcome to Jana Lovelock's Express web server!</h1>
+            <p>Click <a href="/name">here</a> to go to the /name directory.</p>
+        </body>
+    </html>
+`);
+});
 
 app.use('/', require('./routes'));
 app.use(bodyParser.json());
