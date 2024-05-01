a003a39b27da1b0a6c1e8e6007bb6b59a044494e
Janalovelock <janalovelock@gmail.com>
2024-05-01 15:14:50 -0500

    adding mongodb

--- routes/nameRoute.js
+++ routes/nameRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const baseController = require('../controllers');
 router.get('/name', baseController.getName);
-
+router.use('/contacts', require('./contacts'))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 const nameRoute = require('./routes/nameRoute');
+const PORT = process.env.PORT || 3000;
 
+// app.get('/', (req, res) => {
+//     res.send( `
+//     <html>
+//         <head>
+//             <title>Home Page</title>
+//         </head>
+//         <body>
+//             <h1>Welcome to Jana Lovelock's Express web server!</h1>
+//             <p>Click <a href="/name">here</a> to go to the /name directory.</p>
+//         </body>
+//     </html>
+// `);
+// });
 
-app.get('/', (req, res) => {
-    res.send( `
-    <html>
-        <head>
-            <title>Home Page</title>
-        </head>
-        <body>
-            <h1>Welcome to Jana Lovelock's Express web server!</h1>
-            <p>Click <a href="/name">here</a> to go to the /name directory.</p>
-        </body>
-    </html>
-`);
-  });
-  app.use('/', nameRoute);
+app.use('/', require('./routes'));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
 
-  const PORT = process.env.PORT || 3000; // Use the PORT environment variable if available, otherwise use port 3000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(PORT, () => {
+            console.log(`connected to DB and listening on ${PORT}`);
+        });
+    }
 });
